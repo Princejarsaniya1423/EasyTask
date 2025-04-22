4ceb6dfa7cbc27790cb522c95bedd78d75fd0e82
PrinceJarsaniya <155825598+Princejarsaniya14@users.noreply.github.com>
2025-04-22 21:10:03 +0530

    show name while click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-user';
+import { CommonModule } from '@angular/common';
+import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UserComponent, CommonModule, TasksComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  users = DUMMY_USERS;
   title = 'EasyTask';
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, output, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-user',
+  imports: [CommonModule],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
+
+  get imagePath() {
+    return 'assets/user/' + this.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
+}
