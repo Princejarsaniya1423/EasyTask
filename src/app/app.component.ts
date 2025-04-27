40d0e4f721db1063fb18cf9de0470cd569f03f40
PrinceJarsaniya <155825598+Princejarsaniya14@users.noreply.github.com>
2025-04-27 16:35:40 +0530

    Added css code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { TasksComponent } from './tasks/tasks.component';
 export class AppComponent {
   users = DUMMY_USERS;
   title = 'EasyTask';
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,17 @@
 import { Component, EventEmitter, Input, output, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 @Component({
   selector: 'app-user',
   imports: [CommonModule],
@@ -8,17 +19,18 @@ import { CommonModule } from '@angular/common';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input() user!: User;
   @Output() select = new EventEmitter<string>();
   // select = output<string>();
 
   get imagePath() {
-    return 'assets/user/' + this.avatar;
+    return 'assets/user/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
